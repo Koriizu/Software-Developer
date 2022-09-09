9b0042c5408fba7b4226607bd9a143e035ef41fd
Koriizu <arda.koksal05@hotmail.com>
2022-09-09 14:03:45 +0200

    derde commit

---
+++ Software Developer/Hello Python/demo.py
@@ -0,0 +1,11 @@
+lengte = input('lengte: ')
+lengte = float(lengte)
+
+breedte = input('breedte: ')
+breedte = float(breedte)
+
+opp = lengte * breedte
+omtrek = 2* (lengte + breedte)
+
+print('Oppervlakte. wordt', opp) # Deze instructie print het antwoord voor het oppervlakte.
+print ('Omtrek wordt', omtrek) # Deze instructie print het antwoord voor de omtrek.
